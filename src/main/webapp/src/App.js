39af13e4bd5f8de68f3e4b31f290d7c4ba8f0a7c
catherinehee <111953841+catherinehee@users.noreply.github.com>
2023-11-22 16:04:45 -0800

    integrating react to run frontend @ localhost:xxxx/index.html

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,9 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Helmet } from "react-helmet"
+import Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
+
 
 function App() {
+  const [stompClient, setStompClient] = useState(null);
+   const [codeEditorValue, setCodeEditorValue] = useState('');
+
+  useEffect(() => {
+    const connect = () => {
+      const socket = new SockJS('/realtime');
+      const client = Stomp.over(socket);
+      client.connect({}, (frame) => {
+        console.log('Connected: ' + frame);
+        client.subscribe('/topic/updates', (update) => {
+          setCodeEditorValue(update.body);
+        });
+      });
+      setStompClient(client);
+    };
+
+    const sendEdit = () => {
+      const text = codeEditorValue;
+      stompClient.send('/app/editCode', {}, text);
+    };
+
+    const codeEditorInputHandler = (event) => {
+      setCodeEditorValue(event.target.value);
+    };
+
+    const setCodeEditorValue = (value) => {
+      // Implement any necessary logic when setting the code editor value
+      document.getElementById('codeEditor').value = value;
+    };
+
+    document.getElementById('codeEditor').addEventListener('input', sendEdit);
+
+    connect();
+
+    // Cleanup event listener on component unmount
+    return () => {
+      document.getElementById('codeEditor').removeEventListener('input', sendEdit);
+    };
+  }, [stompClient]);
   return (
     <div className="App">
+        <Helmet>
+            <title>Real-Time Editing</title>
+                <script src="https://cdn.jsdelivr.net/npm/sockjs-client/dist/sockjs.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/stomp-websocket/lib/stomp.min.js"></script>
+        </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +67,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <h2>Real-Time Code Editor</h2>
+      <textarea id="codeEditor" rows="10" cols="30"></textarea>
+
     </div>
   );
 }
