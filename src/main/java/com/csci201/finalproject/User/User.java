3515254abf27f8fed3d31be1773cd6e49752e4dd
catherinehee <111953841+catherinehee@users.noreply.github.com>
2023-11-24 00:40:55 -0800

    updated some user controller stuff + added CORS

--- src/main/java/com/csci201/finalproject/User/User.java
+++ src/main/java/com/csci201/finalproject/User/User.java
@@ -13,28 +13,6 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    private String username;
-    private String password;
-
+    private String id;
     private List<String> documents;
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-    public String getPassword() {
-        return password;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/csci201/finalproject/User/UserController.java
+++ src/main/java/com/csci201/finalproject/User/UserController.java
@@ -3,16 +3,10 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api")
 public class UserController {
 
@@ -25,12 +19,19 @@ public class UserController {
         return userService.saveUser(user);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000") // TODO: might delete
     @GetMapping("/users/{username}")
     public User getUser(@PathVariable String username) throws ExecutionException, InterruptedException {
 
         return userService.getUserDetailsByUsername(username);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000") // TODO: might delete
+    @GetMapping("/users/{userid}/documents")
+    public List<String> getDocumentsByUser(@PathVariable String userid) throws ExecutionException, InterruptedException {
+
+        return userService.getDocumentsByUser(userid);
+    }
     @GetMapping("/users")
     public List<User> getAllUsers() throws ExecutionException, InterruptedException {
 
