de1b7a1714311ba6097d9a4b1e96b6be9239962b
catherinehee <111953841+catherinehee@users.noreply.github.com>
2023-11-25 21:21:12 -0800

    filesystem styling

--- src/main/webapp/src/pages/FileSystem.js
+++ src/main/webapp/src/pages/FileSystem.js
@@ -1,9 +1,8 @@
-
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NavBar from '../components/navBar';
 import { useParams, useNavigate } from 'react-router-dom';
-
+import '../css/FileSystem.css';
 
 function FileSystem() {
     let { uid } = useParams();
@@ -11,52 +10,49 @@ function FileSystem() {
     const [newDocumentName, setNewDocumentName] = useState("");
     const navigate = useNavigate();
 
-    // render header: with current user info -> user id
     useEffect(() => {
         const retrieveDocuments = () => {
-            const retrieveDocumentsApi =  `http://localhost:8080/api/users/${uid}/documents`
-            console.log(retrieveDocumentsApi);
+            const retrieveDocumentsApi = `http://localhost:8080/api/users/${uid}/documents`;
             axios.get(retrieveDocumentsApi)
                 .then((response) => response.data)
                 .then((data) => {
                     setDocuments(data);
                 })
                 .catch((error) => {
-                    console.log(error);
-                })
-
-        }
-        console.log("retrieving documents");
+                    console.error(error);
+                });
+        };
         retrieveDocuments();
-    }, [uid])
+    }, [uid]);
 
     const openDocument = (documentId) => {
-        navigate(`/documents/edit/${documentId}`)
-    }
+        navigate(`/documents/edit/${documentId}`);
+    };
 
     const addDocument = (documentName) => {
-            const addDocumentApi = `http://localhost:8080/api/documents/${uid}/${documentName}/add`;
+                const addDocumentApi = `http://localhost:8080/api/documents/${uid}/${documentName}/add`;
 
-            axios.post(addDocumentApi )
-                .then((response) => {
-                    const newDocumentId = response.data;
+                axios.post(addDocumentApi )
+                    .then((response) => {
+                        const newDocumentId = response.data;
 
-                    // Update documents list with the new document ID
-                    setDocuments((prevDocuments) => [...prevDocuments, newDocumentId]);
+                        // Update documents list with the new document ID
+                        setDocuments((prevDocuments) => [...prevDocuments, newDocumentId]);
 
-                    const updateUserDocumentsApi = `http://localhost:8080/api/users/${uid}/documents/${newDocumentId}/add`;
-                    axios.patch(updateUserDocumentsApi)
-                    .then((updateResp) => {
-                        console.log(updateResp.data);
+                        const updateUserDocumentsApi = `http://localhost:8080/api/users/${uid}/documents/${newDocumentId}/add`;
+                        axios.patch(updateUserDocumentsApi)
+                        .then((updateResp) => {
+                            console.log(updateResp.data);
+                        })
+                        .catch((error) => {
+                            console.error(error);
+                        });
                     })
                     .catch((error) => {
-                        console.error(error);
+                        console.log(error);
                     });
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-            };
+                };
+
 
         const removeDocument = (documentId) => {
                     const removeDocumentApi = `http://localhost:8080/api/users/${uid}/documents/${documentId}/delete`;
@@ -74,45 +70,39 @@ function FileSystem() {
             useEffect(() => { // runs every time documents gets updated (re-renders page documents page)
                 console.log('Documents have been updated:', documents);
                 }, [documents]);
+
     return (
-        <div>
-
-            {/* Render header with current user info */}
-            <NavBar uid={uid}/>
-
-            {/* Render documents */}
-            <h3>Documents:</h3>
-            <ul>
-                  {Array.isArray(documents) ? (
-                    documents.map((documentId, index) => (
-                      <li key={index}>
-                        {documentId}
-                        <button onClick={() => removeDocument(documentId)}>Remove Document</button>
-                        <button onClick={() => openDocument(documentId)}>Open Document</button>
-                      </li>
-                    ))
-                  ) : (
-                    <p>Loading documents...</p>
-                  )}
-            </ul>
-
-
-             {/* Add Document form */}
-            <div>
-                <label htmlFor="newDocumentName">New Document Name:</label>
+        <div className="file-system-container">
+            <NavBar uid={uid} />
+            <div className="add-document-bar">
                 <input
-                type="text"
-                id="newDocumentName"
-                value={newDocumentName}
-                onChange={(e) => setNewDocumentName(e.target.value)}
+                    type="text"
+                    id="newDocumentName"
+                    placeholder="Enter document name"
+                    value={newDocumentName}
+                    onChange={(e) => setNewDocumentName(e.target.value)}
                 />
-                <button onClick={() => addDocument(newDocumentName)}>Add Document</button>
+                <button id="add-document-button" onClick={() => addDocument(newDocumentName)}>Add Document</button>
             </div>
+            <div className="documents-list">
+                <h3>Documents:</h3>
+                <ul>
+                    {
+                    Array.isArray(documents) ? (
+                    documents.map((documentId, index) => (
+                        <li key={index} className="document-item">
+                            <span className="document-name">{documentId}</span>
+                            <button onClick={() => openDocument(documentId)}>View</button>
+                            <button onClick={() => removeDocument(documentId)}>Delete</button>
+                        </li>
+                        ) )):
+                        <p>Loading documents... </p>
 
+                    }
+                </ul>
+            </div>
         </div>
     );
-
-
 }
 
 export default FileSystem;
